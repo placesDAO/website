1da7cd09f88e73d1e1dc80b4aecbbd9c2f517dd9
Derek Reynolds <derekr@users.noreply.github.com>
2021-10-14 10:28:59 -0700

    Add ascii art
    ```
    ████░░░░░░░░░░░░░░░░░░░░░░░░░████
    ██░░░░░░░██████░░██████░░░░░░░░██
    ░░░░░░░██████████████████░░░░░░░░
    ░░░░░████████      ████████░░░░░░
    ░░░░░██████  ██████  ██████░░░░░░
    ░░░░░██████  ██████  ██████░░░░░░
    ░░░░░░░████  ██████  ████░░░░░░░░
    ░░░░░░░░░████      ████░░░░░░░░░░
    ░░░░░░░░░░░██████████░░░░░░░░░░░░
    ░░░░░░░░░░░░░██████░░░░░░░░░░░░░░
    ██░░░░░░░░░░░░░██░░░░░░░░░░░░░░██
    ████░░░░░░░░░░░░░░░░░░░░░░░░░████
    ```

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { mapPinAscii } from '../ascii/map-pin'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	console.log(mapPinAscii, 'font-family: mono')
+	return <Component {...pageProps} />
 }
 export default MyApp
