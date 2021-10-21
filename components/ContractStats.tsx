1fd2ccdca979519549ca7a68e3efcc2cd912c317
Derek Reynolds <derekr@users.noreply.github.com>
2021-10-20 19:54:15 -0700

    Prod config

--- components/ContractStats.tsx
+++ components/ContractStats.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
 import Web3 from 'web3'
 
-import contractAbi from '../contract-abi.dev.json'
+import contractAbi from '../contract-abi.json'
 
-const PLACES_CONTRACT_ADDRESS = '0xA2d5b3C437e70BF1d61e647fa391E1C67f92EcdA'
+const PLACES_CONTRACT_ADDRESS = '0x4C1965D8388deFf39a977a89BDF317A9A1DC40b5'
 
 const web3 = new Web3()
 web3.eth.setProvider(
-	'wss://rinkeby.infura.io/ws/v3/865185249fa646ceadb4b51b09fe39a4',
+	'wss://mainnet.infura.io/ws/v3/865185249fa646ceadb4b51b09fe39a4',
 )
 const contract = new web3.eth.Contract(
 	contractAbi as any,
@@ -15,18 +15,23 @@ const contract = new web3.eth.Contract(
 )
 
 export const ContractStats = () => {
-	const [supply, setSupply] = useState<number | null>(null)
-	const [price, setPrice] = useState<string | null>(null)
+	const [totalSupply, setTotalSupply] = useState<number | null>(null)
+	const [placeSupply, setPlaceSupply] = useState<number | null>(null)
+	const [mintFee, setMintFee] = useState<string | null>(null)
 
 	useEffect(() => {
-		contract.methods.totalSupply().call().then(setSupply)
-		contract.methods.getMintFeeInWei().call().then(setPrice)
+		contract.methods.totalSupply().call().then(setTotalSupply)
+		contract.methods.getPlaceSupply().call().then(setPlaceSupply)
+		contract.methods.getMintFeeInWei().call().then(setMintFee)
 	}, [])
 
 	return (
 		<>
-			<p>Mint Fee: {price !== null ? price : 'loading balance…'} ETH</p>
-			<p>{supply ? supply : 'loading supply…'} / 500 minted</p>
+			<p>Mint Fee: {mintFee !== null ? mintFee : 'loading balance…'} ETH</p>
+			<p>
+				{totalSupply !== null ? totalSupply : 'loading available supply…'} /{' '}
+				{placeSupply !== null ? placeSupply : 'loading total supply…'} minted
+			</p>
 		</>
 	)
 }
