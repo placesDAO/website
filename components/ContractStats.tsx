fc0626c645f90c9b5af792d16421f7b82c78b01d
Derek Reynolds <derekr@me.com>
2022-08-23 12:40:20 -0700

    Replace web3 calls with wagmi

--- components/ContractStats.tsx
+++ components/ContractStats.tsx
@@ -1,55 +1,40 @@
-import { useEffect, useState } from 'react'
-import Web3 from 'web3'
-
-import contractAbi from '../contract-abi.json'
-
-const PLACES_CONTRACT_ADDRESS = '0xC9CA129DC3a299aF68A215d85771630aec4C3C2b'
-const PREVIOUS_MINT_COUNT = 500
-
-const web3 = new Web3()
-web3.eth.setProvider(process.env.NEXT_PUBLIC_PROVIDER_ADDRESS as string)
-const contract = new web3.eth.Contract(
-	contractAbi as any,
-	PLACES_CONTRACT_ADDRESS,
-)
-
-// const getCurrentTotalSupply = (totalSupply: number) => {
-// 	if (totalSupply >= PREVIOUS_MINT_COUNT)
-// 		return totalSupply - PREVIOUS_MINT_COUNT
-
-// 	return totalSupply
-// }
-
-// const getCurrentPlaceSupply = (placeSupply: number) => {
-// 	if (placeSupply >= PREVIOUS_MINT_COUNT)
-// 		return placeSupply - PREVIOUS_MINT_COUNT
-
-// 	return placeSupply
-// }
+import { useContractReads } from 'wagmi'
+import { BigNumberish, utils } from 'ethers'
+import { placesABI } from '../lib/places-abi'
 
 export const ContractStats = () => {
-	const [totalSupply, setTotalSupply] = useState<number | null>(null)
-	const [placeSupply, setPlaceSupply] = useState<number | null>(null)
-	const [mintFee, setMintFee] = useState<string | null>(null)
+	const { data, isLoading, isError } = useContractReads({
+		contracts: [
+			{
+				addressOrName: process.env.NEXT_PUBLIC_WEB3_PLACES_CONTRACT_ADDRESS,
+				contractInterface: placesABI,
+				functionName: 'totalSupply',
+			},
+			{
+				addressOrName: process.env.NEXT_PUBLIC_WEB3_PLACES_CONTRACT_ADDRESS,
+				contractInterface: placesABI,
+				functionName: 'getPlaceSupply',
+			},
+			{
+				addressOrName: process.env.NEXT_PUBLIC_WEB3_PLACES_CONTRACT_ADDRESS,
+				contractInterface: placesABI,
+				functionName: 'getMintFeeInWei',
+			},
+		],
+		watch: true,
+	})
 
-	useEffect(() => {
-		contract.methods.totalSupply().call().then(setTotalSupply)
-		contract.methods.getPlaceSupply().call().then(setPlaceSupply)
-		contract.methods.getMintFeeInWei().call().then(setMintFee)
-	}, [])
+	// Just inifintely load on error for now.
+	if (isLoading || isError) return <p>Loading Places Stats</p>
 
 	return (
 		<>
-			<p>
-				Mint Fee:{' '}
-				{mintFee !== null
-					? web3.utils.fromWei(mintFee, 'ether')
-					: 'loading balance…'}{' '}
+			<p style={{ marginBottom: '0' }}>
+				{utils.formatEther(data?.[2] as BigNumberish)}
 				ETH
 			</p>
 			<p>
-				{totalSupply !== null ? totalSupply : 'loading available supply…'} /{' '}
-				{placeSupply !== null ? placeSupply : 'loading total supply…'} minted
+				{`${data?.[0]}`} / {`${data?.[1]}`} minted
 			</p>
 		</>
 	)
